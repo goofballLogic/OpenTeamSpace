04c146b3a17580eae06b96c089fee234d87b3fcf
Andrew Stewart Gibson <andrew@goofballlogic.com>
2018-07-24 15:52:10 +0000

    added dependencies on tc2-

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./Home.css";
 const Home = ( { className } ) => <article className={`home ${className || ""}`}>
 
-    <video src="/Very-Open-Space.mp4" autoPlay loop p-oster="/Very-Open-Space.jpg"></video>
+    <div className="splash">
+        <video src="/Very-Open-Space.mp4" autoPlay loop p-oster="/Very-Open-Space.jpg"></video>
+    </div>
     <p>asdf</p>
 
 </article>;
