876d2f20b38ebac676ba996afa9970fc268c980e
Andrew Stewart Gibson <andrew@goofballlogic.com>
2018-10-06 10:26:00 +0000

    meta file generated

--- scripts/extract-version.js
+++ scripts/extract-version.js
@@ -4,4 +4,4 @@ const p = require( "../package.json" );
 const fs = require( "fs" );
 const metaFile = path.resolve( __dirname, "../src/meta.json" );
 fs.writeFileSync( metaFile, JSON.stringify( { version: p.version }, null, 3 ) );
-child_process.spawn( "git commit -a -m 'meta file generated'" ); 
+child_process.exec( "git commit -a -m 'meta file generated'" ); 
