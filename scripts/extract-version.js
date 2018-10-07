4a3adde42a13be6b4eca6d4e99b627d9af765da5
Andrew Stewart Gibson <andrew@goofballlogic.com>
2018-10-07 18:27:08 +0000

    evup

--- scripts/extract-version.js
+++ scripts/extract-version.js
@@ -3,5 +3,5 @@ const child_process = require( "child_process" );
 const p = require( "../package.json" );
 const fs = require( "fs" );
 const metaFile = path.resolve( __dirname, "../src/meta.json" );
-fs.writeFileSync( metaFile, JSON.stringify( { version: p.version }, null, 3 ) );
+fs.writeFileSync( metaFile, JSON.stringify( { version: `${p.version}-alpha` }, null, 3 ) );
 child_process.exec( "git commit -a -m 'meta file generated'" ); 
